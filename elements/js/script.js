725888ea21099ed574f03127359a954b94be4e53
PopshaNazar <nazarpopsha@gmail.com>
2025-04-27 18:31:14 +0300

    actions with elements

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,23 +1,43 @@
 'use strict';
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'),
+    oneHeart = wrapper.querySelector('.heart');
+// console.dir(box);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-console.log(box);
+box.style.cssText = `background-color: blue; width: 500px`;
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = 'red';
 
-const btns = document.getElementsByTagName('button')[1];
-
-console.log(btns);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('hi');
 
+div.classList.add('black');
+// document.body.append(div);
+wrapper.append(div);
+
+// wrapper.prepend(div);
+// hearts[0].after(div);
+
+// circles[0].remove();
+
+hearts[0].replaceWith(circles[0]);
+// circles[0].replaceWith(hearts[0]);
+
+// div.innerHTML = "<h1>Hello World</h1>";
+div.textContent = "Hello";
+
+div.insertAdjacentHTML("beforebegin", '<h2>Hello</h2>')
